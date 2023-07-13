8746fa1bc27be3fb3eaecb7d30edf374314be5ee
Hiperiosity <anastasy_ya@mail.ru>
2023-07-13 17:56:43 +0300

    15 пр

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,17 +4,14 @@ import './index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import { hren } from 'hren';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-
     <BrowserRouter>
-
       <App/>
-
     </BrowserRouter>
-    
   </React.StrictMode>
 );
 reportWebVitals();
